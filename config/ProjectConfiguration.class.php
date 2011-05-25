537d22fcad14bf41468e39ab51d207df92a674a5
unknown <bgh24318@.asian.ad.aricent.com>
2011-05-25 14:50:16 +0530

    Initial Files added
    Signed-off-by: unknown <bgh24318@.asian.ad.aricent.com>

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'E://development//sfprojects//SBTM//lib//vendor//symfony//lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
