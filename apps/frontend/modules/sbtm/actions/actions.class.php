27c7e4f66e3b45c6e16d125c0a735d2531ad8887
mohamedsithik <mohamed.wea>
2011-06-15 15:45:47 +0530

    added logic for file upload fix #13

--- apps/frontend/modules/sbtm/actions/actions.class.php
+++ apps/frontend/modules/sbtm/actions/actions.class.php
@@ -15,15 +15,30 @@ class sbtmActions extends sfActions
     $this->project_category = Doctrine_Core::getTable('ProjectCategory')
       ->createQuery('a')
      ->execute();
-    
+
   }
+
+  public function executeReporting(sfWebRequest $request)
+  {
+
+  }
+  public function executeBurndownchart(sfWebRequest $request)
+  {
+      $response = $this->getResponse();
+
+    // HTTP headers
+    $response->setContentType('image/png');
+
+
+  }
+
 
   public function executeLogout(sfWebRequest $request)
   {
    $this->getUser()->getAttributeHolder()->clear();
   $this->redirect('sbtm/index');
   }
-  
+
   public function executeProjectadmin(sfWebRequest $request)
   {
 
@@ -31,7 +46,7 @@ class sbtmActions extends sfActions
       ->createQuery('a')
       ->execute();
   }
-  
+
   public function executeUseradmin(sfWebRequest $request)
   {
 
@@ -39,19 +54,20 @@ class sbtmActions extends sfActions
       ->createQuery('a')
      ->execute();
   }
-  
+
   public function executeSessions(sfWebRequest $request)
   {
       $this->project_id = Doctrine_Core::getTable('ProjectCategory')
       ->createQuery('a')
-              ->where('a.name = ?',$this->getUser()->getAttribute('project') )
+      ->where('a.name = ?',$this->getUser()->getAttribute('project') )
      ->execute();
-foreach ($this->project_id as $projectid): 
-   $dbprojectID =$projectid->getId();
-endforeach;
+           foreach ($this->project_id as $projectid):
+           $dbprojectID =$projectid->getId();
+           endforeach;
    $this->sessions = Doctrine_Core::getTable('Sessions')
       ->createQuery('a')
            ->where('a.project_id = ?',$dbprojectID)
+           ->andWhereIn('a.status_id',array(1))
            //->where('a.ready=?','yes')
      ->execute();
   }
@@ -63,9 +79,10 @@ endforeach;
      ->where('a.id = ?',$request->getParameter('id'))
      ->execute();
   }
-  
+
   public function executeLogin(sfWebRequest $request)
   {
+
     $this->user = $request->getParameter('username');
     $this->pass = $request->getParameter('password');
     $this->project = $request->getParameter('project_action');
@@ -75,33 +92,40 @@ endforeach;
            ->createQuery('l')
            ->where('l.username = ?',$this->user )
            ->execute();
-    foreach ($this->logins as $login): 
+    foreach ($this->logins as $login):
     $dbuser=$login->getUsername();
     $dbpass=$login->getPassword();
-    $dbrole=$login->getRole();    
+    $dbrole=$login->getRole();
+    $dblock=$login->getIslocked();
     endforeach;
      $this->getUser()->setAttribute('adminrole', $dbrole);
+
     if($this->user==$dbuser && $this->pass==$dbpass && $this->project != ""){
         $this->getUser()->setAttribute('logindone', 'logindone');
+        if($dblock!=''){
+        $this->getUser()->setAttribute('error', 'User '.$this->user.' locked please contact administrator');
+        $this->redirect('sbtm/index');
+}
+
         if($this->project=="newproject" && $dbrole=="Admin"){
-           
+
             $this->redirect('ProjectCategory/new');
         }
         if($this->project=="newproject" && $dbrole=="User"){
-           
+           $this->getUser()->setAttribute('error', 'User '.$this->user.' not allowed to create a new project');
             $this->redirect('sbtm/index');
         }
-        
-        
+
+
         }
     else{
+        $this->getUser()->setAttribute('error', 'Username/Password not Valid');
         $this->redirect('sbtm/index');
-        
+
         }
 
-    
    }
-  
+
 
   public function executeShow(sfWebRequest $request)
   {
@@ -162,11 +186,19 @@ endforeach;
       $this->redirect('sbtm/edit?id='.$logins->getId());
     }
   }
-  
+
   public function executeUpload(sfWebRequest $request)
 {
-$target_path = "uploads/";
-$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+       $dirname = $this->getUser()->getAttribute('project'); 
+    $filename = "uploads/{$dirname}/"; 
+    
+    if (!file_exists($filename)) { 
+       mkdir("uploads/{$dirname}", 0777); 
+        echo "The directory {$dirname} was successfully created."; 
+    } 
+      
+$target_path = "uploads/{$dirname}/";
+$target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
 
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
      $sessionupdate = Doctrine_Core::getTable('Sessions')->find(array($this->getUser()->getAttribute('id')));
@@ -181,13 +213,109 @@ if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
     $this->getUser()->setAttribute('uploadmessage', 'There was an error uploading the file, please try again! ');
 }
 
-        
-        
-$this->redirect('sbtm/sessions');    
+
+
+$this->redirect('sbtm/sessions');
 }
  public function executeUploads(sfWebRequest $request)
 {
 $this->getUser()->setAttribute('id',$request->getParameter('id'));
 }
+
+ public function executeUsermysession(sfWebRequest $request)
+  {
+     $this->project_id = Doctrine_Core::getTable('ProjectCategory')
+      ->createQuery('a')
+              ->where('a.name = ?',$this->getUser()->getAttribute('project') )
+     ->execute();
+foreach ($this->project_id as $projectid):
+   $dbprojectID =$projectid->getId();
+endforeach;
+   $this->sessions = Doctrine_Core::getTable('Sessions')
+      ->createQuery('a')
+           ->where('a.tester = ?',$this->getUser()->getAttribute('username'))
+                   ->andWhere('a.project_id = ?',$dbprojectID)
+           ->andWhereIn('a.status_id',array(3,5))
+           //->where('a.ready=?','yes')
+     ->execute();
+  }
+
+
+public function executeAdminmysession(sfWebRequest $request)
+  {
+     $this->project_id = Doctrine_Core::getTable('ProjectCategory')
+      ->createQuery('a')
+              ->where('a.name = ?',$this->getUser()->getAttribute('project') )
+     ->execute();
+foreach ($this->project_id as $projectid):
+   $dbprojectID =$projectid->getId();
+endforeach;
+   $this->sessions = Doctrine_Core::getTable('Sessions')
+      ->createQuery('a')
+           ->where('a.tester != ?','')
+           ->andWhere('a.status_id=?','2')
+           ->andWhere('a.project_id = ?',$dbprojectID)
+     ->execute();
+  }
+
+
+  public function executeReviewsubmit(sfWebRequest $request)
+{
+      $dirname = $this->getUser()->getAttribute('project'); 
+      $target_path = "uploads/{$dirname}/";
+$target_path = $target_path . $this->getUser()->getAttribute('filename');
+
+if(file_put_contents($target_path, $request->getParameter('quote'))) {
+    $stat_id=Doctrine_Core::getTable('Status')
+    ->createQuery('a')
+           ->where('a.name = ?',$request->getparameter('status_action'))
+            ->execute();
+    
+    foreach ($stat_id as $statid):
+   $dbstatID =$statid->getId();
+endforeach;
+$this->logMessage($request->getparameter('status_action').'sithik'.$dbstatID);
+    $sessionupdate = Doctrine_Core::getTable('Sessions')->find(array($this->getUser()->getAttribute('id')));
+      $usertest=$this->getUser()->getAttribute('username');
+      $sessionupdate->setStatusId($dbstatID);
+      //$sessionupdate->setTester($usertest);
+      $sessionupdate->save();
+      $urlRefresh = "sessions";
+      header("Refresh: 1; URL=\"" . $urlRefresh . "\"");
+    $this->getUser()->setAttribute('uploadmessage', 'The file '.  basename( $_FILES['uploadedfile']['name']).'has been uploaded');
+} else{
+    $this->getUser()->setAttribute('uploadmessage', 'There was an error uploading the file, please try again! ');
+}
+
+
+
+$this->redirect('sbtm/adminmysession');
+}
+
+
+public function executeSessionlist(sfWebRequest $request)
+  {
+    $this->project_id = Doctrine_Core::getTable('ProjectCategory')
+      ->createQuery('a')
+              ->where('a.name = ?',$this->getUser()->getAttribute('project') )
+     ->execute();
+foreach ($this->project_id as $projectid):
+   $dbprojectID =$projectid->getId();
+endforeach;
+    $this->approved_sessions = Doctrine_Core::getTable('Sessions')
+      ->createQuery('a')
+            ->where('a.status_id=?','4')
+            ->andWhere('a.project_id = ?',$dbprojectID)
+     ->execute();
+    
+    
+    $this->progress_sessions = Doctrine_Core::getTable('Sessions')
+      ->createQuery('a')
+            ->where('a.status_id!=?','4')
+            ->andWhere('a.project_id = ?',$dbprojectID)
+     ->execute();
+
+  }
+
 
 }
