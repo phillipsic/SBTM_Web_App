ea71c5d2dca186670ae31633db87a1c80d2b8506
mohamedsithik <mohamed.wea>
2011-06-30 14:17:10 +0530

    Fix for Burn down chart

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once 'E://development//sfprojects//SBTM//lib//vendor//symfony//lib/autoload/sfCoreAutoload.class.php';
+require_once 'E://development//PQASBTM//lib//vendor//symfony//lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
-  {$this->setWebDir('E://development//PQASBTM//web');
+  {
     $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('stOfcPlugin');
   }
 }
