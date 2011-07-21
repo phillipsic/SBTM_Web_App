112886da5a37d7d343db62f233162683f66c4f37
mohamedsithik <mohamed.wea>
2011-07-21 12:59:26 +0530

    Fix for #17 added coverage and template

--- apps/frontend/modules/sbtm/templates/viewcoverageSuccess.php
+++ apps/frontend/modules/sbtm/templates/viewcoverageSuccess.php
@@ -37,7 +37,9 @@ function madeSelection(elem, helperMsg){
     <th> 
 <textarea rows="20" cols="100" name="quote" wrap="physical" readonly="readonly"><?php echo $sf_user->getAttribute('theData') ?></textarea>
     </th>
-
+    <th>
+<ul class="sf_admin_actions">
+      <li class="sf_admin_action_previous"><a href="<?php echo url_for('sbtm/managecoverage') ?>">Back</a></li>    </ul></th>
     </tr>
     </thead>
 </table>
--- apps/frontend/modules/sbtm/templates/viewtemplateSuccess.php
+++ apps/frontend/modules/sbtm/templates/viewtemplateSuccess.php
@@ -29,7 +29,6 @@ function madeSelection(elem, helperMsg){
 
 </script>
 <div id="sf_admin_container">
-<form action="" method="post" onsubmit='return formValidator()'>
 <titles>view Template</titles>
     <table>
 <thead>
@@ -37,9 +36,10 @@ function madeSelection(elem, helperMsg){
     <th> 
 <textarea rows="20" cols="100" name="quote" wrap="physical" readonly="readonly"><?php echo $sf_user->getAttribute('theData') ?></textarea>
     </th>
-
+    <th>
+<ul class="sf_admin_actions">
+      <li class="sf_admin_action_previous"><a href="<?php echo url_for('sbtm/managetemplate') ?>">Back</a></li>    </ul></th>
     </tr>
     </thead>
 </table>
-</form>
 </div>
