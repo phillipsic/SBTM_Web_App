3b0315ad847cf1c014fbf1c9031f5cce0bc7a51f
Ian Phillips <ian.phillips@Comverse.com>
2012-04-03 18:33:48 +0200

    Code for issue #68

--- apps/frontend/modules/sbtm/templates/sessionsSuccess.php
+++ apps/frontend/modules/sbtm/templates/sessionsSuccess.php
@@ -1,99 +1,126 @@
 <script type="text/javascript">
-/* &lt;![CDATA[ */
-function checkAll()
-{
+    /* &lt;![CDATA[ */
+    function checkAll()
+    {
  
-  var boxes = document.getElementsByTagName('input');
-  for(var index = 0; index < boxes.length; index++){
-      box = boxes[index];
-      if (box.type == 'checkbox' && box.className == 'sf_admin_batch_checkbox') {
-          box.checked = document.getElementById('sf_admin_list_batch_checkbox').checked}
-  } 
-      return true;
-}
-/* ]]&gt; */
+        var boxes = document.getElementsByTagName('input');
+        for(var index = 0; index < boxes.length; index++){
+            box = boxes[index];
+            if (box.type == 'checkbox' && box.className == 'sf_admin_batch_checkbox') {
+                box.checked = document.getElementById('sf_admin_list_batch_checkbox').checked}
+        }
+        return true;
+    }
+    /* ]]&gt; */
 </script>
 <?php echo $sf_user->getAttribute('sithik') ?>
-<?php $admin=$sf_user->getAttribute('adminrole');
-$admin=$sf_user->getAttribute('adminrole');?> 
+<?php $admin = $sf_user->getAttribute('adminrole');
+$admin = $sf_user->getAttribute('adminrole'); ?>
 <div id="sf_admin_container">
     <form method="post" action="">
         <titles>Sessions</titles>
-    <div class="sf_admin_list" style="overflow:auto; width:900px; height: 350px;">
-      <table  border="5">
-           <?php if ($sessions->count()>0){ ?>
-      <thead>
-        <tr>
-          
-          <th class="sf_admin_text sf_admin_list_th_name">
-           Session Name
-          </th>
-          <th class="sf_admin_text sf_admin_list_th_name">
-           Ready For Run
-          </th>
-          <th class="sf_admin_text sf_admin_list_th_name">
-            Status
-           </th>
-           <th class="sf_admin_date sf_admin_list_th_created_at">
-            Created at
-           </th>
-           <th id="sf_admin_list_th_actions">Tester/Actions</th>
-        </tr>
-      </thead>
-      <?php } else{ ?>  
-      <div class="record_error">
-            <?php //$sf_user->getAttributeHolder()->clear();
-            echo 'No record Found'; ?>
-          </div>
-      <?php }  ?> 
-      <tbody>
-          <?php foreach ($sessions as $session): ?>
-            <tr class="sf_admin_row odd">
-            <td class="sf_admin_text sf_admin_list_td_name">
-            <a href="<?php echo url_for('sessions/show?id='.$session->getId()) ?>"><?php echo $session->getSessionname() ?></a></td>
-            <td class="sf_admin_text sf_admin_list_td_name">
-            <?php echo $session->getReady() ?></td>
-            <td class="sf_admin_text sf_admin_list_td_name">
-            <?php echo $session->getStatus() ?></td>
-            <td class="sf_admin_date sf_admin_list_td_created_at">
-             <?php echo $session->getCreatedAt() ?></td>
+        <div class="sf_admin_list" style="overflow:auto; width:900px; height: 350px;">
+            <table  border="5">
+                <?php if ($sessions->count() > 0) {
+ ?>
+                    <thead>
+                        <tr>
 
-            <td>
-            <ul class="sf_admin_td_actions">
-                <?php if ($admin=="Admin"): ?> 
-            <li class="sf_admin_action_edit">
-            <a href="<?php echo url_for('sessions/edit?id='.$session->getId()) ?>">Edit</a>
-            </li>  
-            <li class="sf_admin_action_delete">
-            <?php echo link_to('Delete', 'sessions/delete?id='.$session->get('id'), array('post' => true, 'confirm' => 'Are you sure?')) ?>
-            </li> 
-              <?php endif ?>
-            <?php if ($session->getTester()== null){ ?>
-            <li class="sf_admin_action_download">
-            <a href="<?php echo url_for('sessions/download?id='.$session->getId()) ?>">Download</a>
-            </li> 
-            <?php }
-            else{?>
-               <li class="sf_admin_action_download">
-            <?php echo $session->getTester() ?></li> 
-             <?php } ?>
+                            <th class="sf_admin_text sf_admin_list_th_name">
+                                Session Name
+                            </th>
+                            <th class="sf_admin_text sf_admin_list_th_name">
+                                Ready For Run
+                            </th>
+                            <th class="sf_admin_text sf_admin_list_th_name">
+                                Status
+                            </th>
+                            <th class="sf_admin_date sf_admin_list_th_created_at">
+                                Created at
+                            </th>
+                            <th id="sf_admin_list_th_actions">Tester/Actions</th>
+                        </tr>
+                    </thead>
+<?php } else { ?>
+                    <div class="record_error">
+                    <?php //$sf_user->getAttributeHolder()->clear();
+                    echo 'No record Found'; ?>
+                </div>
+<?php } ?>
+                <tbody>
+<?php foreach ($sessions as $session): ?>
+                    <tr class="sf_admin_row odd">
+                        <td class="sf_admin_text sf_admin_list_td_name">
+                            <a href="<?php echo url_for('sessions/show?id=' . $session->getId()) ?>"><?php echo $session->getSessionname() ?></a></td>
+                        <td class="sf_admin_text sf_admin_list_td_name">
+<?php echo $session->getReady() ?></td>
+                        <td class="sf_admin_text sf_admin_list_td_name">
+<?php echo $session->getStatus() ?></td>
+                        <td class="sf_admin_date sf_admin_list_td_created_at">
+<?php echo $session->getCreatedAt() ?></td>
 
-            <!--li class="sf_admin_action_upload">
-            <a href="<?php echo url_for('sbtm/uploads?id='.$session->getId()) ?>">Upload</a>
-            </li--> 
-            </ul>
-            </td>
-          </tr>
-          <?php endforeach; ?> 
-        </tbody>
-    </table>
-  </div>
-<?php if ($admin=="Admin"): ?> 
-    <ul class="sf_admin_actions">
-      <li class="sf_admin_action_new"><a href="<?php echo url_for('sessions/new') ?>">Add Session</a></li> 
+                        <td>
+                            <ul class="sf_admin_td_actions">
+<?php if ($admin == "Admin"): ?>
+                                <li class="sf_admin_action_edit">
+                                    <a href="<?php echo url_for('sessions/edit?id=' . $session->getId()) ?>">Edit</a>
+                                </li>
+                                <li class="sf_admin_action_delete">
+<?php echo link_to('Delete', 'sessions/delete?id=' . $session->get('id'), array('post' => true, 'confirm' => 'Are you sure?')) ?>
+                                </li>
+<?php endif ?>
 
-      <!--li class="sf_admin_action_upload"><a href="<?php echo url_for('sessions/uploads') ?>">Upload Sessions</a></li-->    </ul>
+
+                                <?php
+                                if ($session->getTester() == null) {
+
+                                    $this->progress_sessions = Doctrine_Core::getTable('Sessions')
+                                                    ->createQuery('a')
+                                                    ->where('a.status_id = ?', '3')
+                                                    ->andWhere('a.tester = ?', $sf_user->getAttribute('username'))
+                                                    ->execute();
+
+                                    if ($this->progress_sessions->count() > 0) {
+                                        echo "<center><font color=\"red\">Session already running</font></center>";
+                                    } else { ?>
+
+                                        <li class="sf_admin_action_download">
+                                        <a href="<?php echo url_for('sessions/download?id=' . $session->getId()) ?>">Download</a>
+                                    </li>
+
+                                   <?php } ?>
+
+    
+
+                                 
+                                <?php } else {
+ ?>
+                                    <li class="sf_admin_action_download">
+                                    <?php echo $session->getTester() ?></li>
+                                <?php } ?>
+
+                                <!--li class="sf_admin_action_upload">
+                                <a href="<?php echo url_for('sbtm/uploads?id=' . $session->getId()) ?>">Upload</a>
+                                </li-->
+                            </ul>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+
+
+
+
+
+
+                            </tbody>
+                        </table>
+                    </div>
+        <?php if ($admin == "Admin"): ?>
+                                    <ul class="sf_admin_actions">
+                                        <li class="sf_admin_action_new"><a href="<?php echo url_for('sessions/new') ?>">Add Session</a></li>
+
+                                  <!--li class="sf_admin_action_upload"><a href="<?php echo url_for('sessions/uploads') ?>">Upload Sessions</a></li-->    </ul>
         <?php endif ?>
     </form>
-  </div>
+</div>
 
