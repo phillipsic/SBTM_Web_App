1ab738a7ec21bbcd4aabc41eec52ee9ed9346c97
Ian Phillips <ian.phillips@Comverse.com>
2012-04-03 15:12:42 +0200

    Clean up of code - removed commented out code

--- apps/frontend/modules/sessions/actions/actions.class.php
+++ apps/frontend/modules/sessions/actions/actions.class.php
@@ -158,85 +158,13 @@ class sessionsActions extends sfActions {
         }
     }
 
-    /* public function executeDownload(sfWebRequest $request) {
-
-      $sessionupdate = Doctrine_Core::getTable('Sessions')->find(array($request->getParameter('id')));
-      $usertest=$this->getUser()->getAttribute('username');
-      $sessionupdate->setStatusId('3');
-      $sessionupdate->setTester($usertest);
-      $sessionupdate->save();
-      $urlRefresh = "sessions";
-      header("Refresh: 1; URL=\"" . $urlRefresh . "\"");
-
-      $linebreaker="\n";
-      $extra="-----------------------------------------------";
-      $chart="CHARTER";
-      $area="AREAS";
-      $start="START";
-      $tester="TESTER";
-      $task="TASK BREAKDOWN";
-      $bugs="BUGS";
-      $issues="ISSUES";
-      $testnotes="TEST NOTES";
-      $datafile="DATA FILES";
-      $taskcontent="#DURATION
-      #TEST DESIGN AND EXECUTION
-      #BUG INVESTIGATION AND REPORTING
-      #SESSION SETUP
-      #CHARTER VS. OPPORTUNITY
-      100/0";
-      $testnotescontent="Build Number -
-      PQA Environment -
-      Document passed and failed tests
-      Assessment of quality?
-      What was not tested?";
-      $notapp="N/A";
-      $this->sessions = Doctrine::getTable('Sessions')->find($request->getParameter('id'));
-      $filename=$this->sessions->getSessionname();
-      $myFile = $this->sessions->getFileSlug();
-      $fh = fopen($myFile, 'w') or die("can't open file");
-      $stringData = $chart.$linebreaker.$extra.$linebreaker.
-      $this->sessions->getCharter().$linebreaker.$linebreaker.
-      $area.$linebreaker.$extra.$linebreaker.str_replace(";", $linebreaker, $this->sessions->getAreas())
-      .$linebreaker.$linebreaker.
-      $start.$linebreaker.$extra.$linebreaker.
-      $linebreaker.$linebreaker.
-      $tester.$linebreaker.$extra.$linebreaker.
-      $linebreaker.$linebreaker.
-      $task.$linebreaker.$extra.$linebreaker.
-      $taskcontent.$linebreaker.$linebreaker.
-      $datafile.$linebreaker.$extra.$linebreaker.
-      $notapp.$linebreaker.$linebreaker.
-      $testnotes.$linebreaker.$extra.$linebreaker.
-      $this->sessions->getTestnotes().$linebreaker.$linebreaker.
-      $bugs.$linebreaker.$extra.$linebreaker.
-      $notapp.$linebreaker.$linebreaker.
-      $issues.$linebreaker.$extra.$linebreaker.
-      $notapp;
-      fwrite($fh, $stringData);
-      $count = $this->sessions->columnCount()."\n";
-      //for ( $column = 0; $counter <= $count; $column++) {
-      // fwrite($fh, $this->sessions->get($column+1));
-      // }
-      fclose($fh);
-      $response = $this->getContext()->getResponse();
-      $response->clearHttpHeaders();
-      $response->addCacheControlHttpHeader('Cache-control', 'must-revalidate, post-check=0, pre-check=0');
-      $response->setContentType('application/octet-stream', true);
-      $response->setHttpHeader('Content-transfer-encoding', 'binary');
-      $response->setHttpHeader('Content-Disposition', 'attachement; filename=' . $this->sessions->getFileSlug() );
-      $response->sendHttpHeaders();
-      $response->setContent(file_get_contents('./'.$myFile, true));
-      unlink('./'.$myFile);
-
-
-
-      return sfView::NONE;
-
-
-      } */
-
+ 
     public function executeDownload(sfWebRequest $request) {
+
+
+        //[Ian]Need to add check here?  Or before we do executeDownload?
+
+
 
         $sessionupdate = Doctrine_Core::getTable('Sessions')->find(array($request->getParameter('id')));
         $usertest = $this->getUser()->getAttribute('username');
