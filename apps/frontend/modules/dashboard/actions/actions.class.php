fcae39ca19006f57c86d2c7379afc422a411c212
Ian Phillips <ian.phillips@Comverse.com>
2012-04-05 16:31:22 +0200

    Added action class - executeAlldropdashboard

--- apps/frontend/modules/dashboard/actions/actions.class.php
+++ apps/frontend/modules/dashboard/actions/actions.class.php
@@ -3,20 +3,40 @@
 /**
  * dashboard actions.
  *
- * @package    PQASBTM
+ * @package    QASBTM
  * @subpackage dashboard
- * @author     Mohamed Sithik
+ * @author     Ian Phillips
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class dashboardActions extends sfActions
-{
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->forward('default', 'module');
-  }
+class dashboardActions extends sfActions {
+
+    /**
+     * Executes index action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeIndex(sfWebRequest $request) {
+        $this->forward('default', 'module');
+    }
+
+    public function executeAlldropdashboard(sfWebRequest $request) {
+
+
+        $this->project_id = Doctrine_Core::getTable('ProjectCategory')
+                        ->createQuery('a')
+                        ->where('a.name = ?', $this->getUser()->getAttribute('project'))
+                        ->execute();
+
+
+        foreach ($this->project_id as $projectid):
+            $dbprojectID = $projectid->getId();
+
+        endforeach;
+
+        $this->sessions = Doctrine_Core::getTable('Sessions')
+                        ->createQuery('a')
+                        ->where('a.project_id = ?', $dbprojectID)
+                        ->execute();
+    }
+
 }
