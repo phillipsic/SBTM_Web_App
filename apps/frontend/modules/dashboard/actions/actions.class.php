750a15b466ee5476be07a7409dc3857210d14797
Ian Phillips <ian.phillips@Comverse.com>
2012-04-05 13:48:26 +0200

    Initial commit of new module - dashboard - issues #73

---
+++ apps/frontend/modules/dashboard/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * dashboard actions.
+ *
+ * @package    PQASBTM
+ * @subpackage dashboard
+ * @author     Mohamed Sithik
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class dashboardActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
